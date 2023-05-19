ba329291f11f835a0a15f8009721625353394e20
Owen Rees <owenrees82@gmail.com>
2023-05-19 13:38:56 +0200

    gh pages

--- src/components/partials/ImageGalleryLayer.jsx
+++ src/components/partials/ImageGalleryLayer.jsx
@@ -1,6 +1,15 @@
 import close from "../../assets/images/icon-close.svg";
+import previous from "../../assets/images/icon-previous.svg";
+import next from "../../assets/images/icon-next.svg";
+import {
+  handleNext,
+  handlePrevious,
+} from "../../handlers/imageGalleryHandlers.js";
 
 const ImageGalleryLayer = ({
+  imageData,
+  imageNumber,
+  setImageNumber,
   image,
   imageThumbnails,
   setShowImageGalleryLayer,
@@ -15,6 +24,20 @@ const ImageGalleryLayer = ({
           onClick={() => setShowImageGalleryLayer(false)}
         />
         <img className="main-image" src={image.image} />
+        <div className="gallery-buttons">
+          <img
+            className="previous-button"
+            src={previous}
+            onClick={() =>
+              handlePrevious(imageNumber, setImageNumber, imageData)
+            }
+          />
+          <img
+            className="next-button"
+            src={next}
+            onClick={() => handleNext(imageNumber, setImageNumber, imageData)}
+          />
+        </div>
         <div className="image-thumbnails">{imageThumbnails()}</div>
       </div>
     </>
